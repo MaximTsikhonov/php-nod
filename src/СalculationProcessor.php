d79f902bc30cc8ba889e3a2d04fb899b41bed5ef
MaximTsikhonov <mateny90889@gmail.com>
2023-08-21 09:31:52 +0300

    init project

---
+++ src/СalculationProcessor.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaximTsikhonov\PhpNod;
+
+class СalculationProcessor
+{
+    public function nod(int $num1, int $num2): int
+    {
+        while ($num1 != $num2)
+        {
+            if ($num1 > $num2) $num1 =  $num1 - $num2;
+            else $num2 = $num2 - $num1;
+        }
+        return $num2;
+    }
+}
